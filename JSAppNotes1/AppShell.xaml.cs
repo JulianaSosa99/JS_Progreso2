4bd0e231ea39613088015f5a314e17beb9be75db
cisco <cisco@UPOAULA10307>
2024-05-29 11:05:19 -0500

    Agregar archivos de proyecto.

---
+++ JSAppNotes1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JSAppNotes1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JSAppNotes1/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace JSAppNotes1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
