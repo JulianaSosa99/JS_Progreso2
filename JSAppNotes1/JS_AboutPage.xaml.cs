845bee99ad61c443f071688622b1fa0036a9a741
JulianaSosa99 <juliana.sosa@udla.edu.ec>
2024-05-29 11:39:26 -0500

    Añadi imagenes y cambio de informacion de clicked

--- JSAppNotes1/JS_AboutPage.xaml.cs
+++ JSAppNotes1/JS_AboutPage.xaml.cs
@@ -8,8 +8,10 @@ public partial class JS_AboutPage : ContentPage
 		InitializeComponent();
 	}
 
-    private async Task JS_Click_BotonAsync(object sender, EventArgs e)
+ 
+    private void LearnMore_Clicked(object sender, EventArgs e)
     {
+
         await Launcher.Default.OpenAsync("https://educacionvirtual.udla.edu.ec/");
     }
 }
