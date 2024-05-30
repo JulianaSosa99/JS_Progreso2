d0ad042f38f11941f5994949cb832a3f23f84722
Kamila <juliana.sosa@udla.edu.ec>
2024-05-29 22:25:01 -0500

    Creacion de la clase JS_About dentro de la carpeta models, y actualización de AboutPage de Views

--- JSAppNotes1/Models/JS_Note.cs
+++ JSAppNotes1/Models/JS_Note.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace JSAppNotes1.Models
 {
     internal class JS_Note { 
-    public string Filename { get; set; }
-    public string Text { get; set; }
+    public string JS_Filename { get; set; }
+    public string JS_Text { get; set; }
     public DateTime Date { get; set; }
 
     
